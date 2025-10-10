2e7dc2f02fb89399f7efad1cc867f910b28033ea
hezhefly <35137227+hezhefly@users.noreply.github.com>
2025-10-10 11:15:32 +0800

    Update __init__.py
    add `S3Storage` class

--- src/agentscope_runtime/sandbox/manager/storage/__init__.py
+++ src/agentscope_runtime/sandbox/manager/storage/__init__.py
@@ -2,9 +2,11 @@
 from .data_storage import DataStorage
 from .local_storage import LocalStorage
 from .oss_storage import OSSStorage
+from .s3_storage import S3Storage
 
 __all__ = [
     "DataStorage",
     "LocalStorage",
     "OSSStorage",
+    "S3Storage"
 ]
